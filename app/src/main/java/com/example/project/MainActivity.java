66e5ae39299d1b22b74019c0402cd5c881f714ce
dianasero <dianalehm.15@gmail.com>
2020-03-28 22:55:51 -0600

    url

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,21 +3,18 @@ package com.example.project;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.widget.*;
+import android.util.Log;
 import android.widget.Toast;
-import java.io.*;
+
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,22 +22,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            petGET();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        WebView myWebView = (WebView) findViewById(R.id.webView);
-        myWebView.loadUrl("http://www.google.com");
+//        Toast toast2 =
+//                Toast.makeText(getApplicationContext(),
+//                        " cone", Toast.LENGTH_SHORT);
+//        toast2.show();
+        peticion getIm = new peticion();
+        getIm.execute("");
+//        try {
+//            funGET();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        WebView myWebView = (WebView) findViewById(R.id.webView);
+//        myWebView.loadUrl("http://www.google.com");
     }
     public void funGET() throws IOException {
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            URL url = new URL("http://localhost:3000/");
+            URL url = new URL("http://192.168.1.76:3000/");
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            System.out.print("Conexion"+ urlConnection.toString());
+            Log.d("d","Conexion"+ urlConnection.toString());
             try {
                 InputStream in = new BufferedInputStream(urlConnection.getInputStream());
                 Toast toast2 =
@@ -64,16 +67,56 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void petGET() throws Exception {
-        URL url = new URL("http://localhost:3000/");
-        StringBuilder  resultado = new StringBuilder();
-        HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
-        conexion.setRequestMethod("GET");
-        BufferedReader  rd = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
-        String linea;
-        while ((linea = rd.readLine())!= null){
-            resultado.append(linea);
+        URL url = null;
+        InputStream in = null;
+        try {
+            url = new URL("http://192.168.1.76:3000/");
+            HttpURLConnection urlConnection = null;
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream());
+
+            Log.d("d--------------------",in.toString());
+            urlConnection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        rd.close();
-        System.out.println(resultado.toString());
+        Toast toast2 =
+                Toast.makeText(getApplicationContext(),
+                        "res---------"+in.toString(), Toast.LENGTH_LONG);
+        toast2.show();
+    }
+}
+
+class peticion extends AsyncTask<String, Integer, Void> {
+
+
+    public Void doInBackground(String... exten) {
+        URL url = null;
+        InputStream in = null;
+        try {
+            url = new URL("http://192.168.1.76:3000/");
+            HttpURLConnection urlConnection = null;
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream());
+            Log.d("d--------------------",in.toString()+"-----"+urlConnection);
+            urlConnection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void onProgressUpdate(Integer... progress) {
+
+    }
+
+
+    public void onPostExecute(Long result) {
+
     }
 }
